b7877f7cbf9569ef52dcf5b0b8fac369455266da
ThainaVisioli <thaina.visioli@aluno.senai.br>
2026-03-09 11:12:39 -0300

    Situação DE aprendizagem 1

---
+++ PBE_SA1/banco.sql
@@ -0,0 +1,13 @@
+create database mvc_livros;
+
+use mvc_livros;
+
+CREATE TABLE LIVROS (
+
+	ID INTEGER AUTO_INCREMENT PRIMARY KEY,
+    TITULO varchar(255),
+    AUTOR varchar(255),
+    ANOPUBLICADO varchar (255)
+    
+    );
+    
